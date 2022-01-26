6811ff44b80d3c5181d9e2cf670f231bc3a451e9
Natã Elienai <nata.elienai@gmail.com>
2022-01-26 15:44:06 -0300

    style: rename NameFilterInput to NameSearchBar and Table to PlanetTable

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { PlanetProvider } from './context/PlanetContext';
 import { FilterProvider } from './context/FilterContext';
-import NameFilterInput from './components/NameFilterInput';
-import Table from './components/Table';
+import NameSearchBar from './components/NameSearchBar';
+import PlanetTable from './components/PlanetTable';
 
 export default function App() {
   return (
     <FilterProvider>
-      <NameFilterInput />
+      <NameSearchBar />
       <PlanetProvider>
-        <Table />
+        <PlanetTable />
       </PlanetProvider>
     </FilterProvider>
   );
--- src/components/NameSearchBar.js
+++ src/components/NameSearchBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useFilter } from '../context/FilterContext';
 
-export default function NameFilterInput() {
+export default function NameSearchBar() {
   const { nameToFilterBy, setNameToFilterBy } = useFilter();
 
   function handleInputChange({ target: { value } }) {
